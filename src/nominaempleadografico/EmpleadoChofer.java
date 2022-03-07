74d647767069f6bcf7127a6424e2f78c78811a81
HiramZednem <Hirammendez000@gmail.com>
2022-03-07 08:16:34 -0600

    Se modifico el formato string para hacerlo mas legible

--- src/nominaempleadografico/EmpleadoChofer.java
+++ src/nominaempleadografico/EmpleadoChofer.java
@@ -10,7 +10,6 @@ public class EmpleadoChofer extends EmpleadoPorComision {
 
     public EmpleadoChofer(String primerNombre, String apellidoPaterno, String numeroSeguroSocial, int SalarioBase, Double RecorridoEnKm,String TipoGasolina) {
         super(primerNombre, apellidoPaterno, numeroSeguroSocial);
-        
         this.RecorridoEnKm = RecorridoEnKm;
         this.SalarioBase = SalarioBase;
         this.TipoGasolina = TipoGasolina;
@@ -22,27 +21,27 @@ public class EmpleadoChofer extends EmpleadoPorComision {
     public void ViaticosyCombustible(Double RecorridoEnKm, Double ViaticosAndCombustible, String TipoGasolina){
         final double precioPorLitro = 0.4;
         
-        final double precioGasolina = (TipoGasolina ==     "Magna")   ? 20.86:
-                                                               (TipoGasolina == "Premium")   ? 21.25:
-                                                                                                                           22.66;
+        final double precioGasolina = (TipoGasolina.equals(    "Magna"))   ? 20.86:
+                                                               (TipoGasolina.equals("Premium"))   ? 21.25:
+                                                                                                                                    22.66;
                 
         double viatico = (RecorridoEnKm < 100) ? 200 : (RecorridoEnKm * 4);
         double gasolina = ( (RecorridoEnKm *precioPorLitro ) * precioGasolina );
                                         
                  
-       //double PrecioViatico = ;
-       setViaticosAndCombustible(viatico+gasolina);           
-        
+       double PrecioViatico = viatico+gasolina;
+       setViaticosAndCombustible(PrecioViatico);           
     }
 
     @Override
     public String toString() {
-        return String.format("%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f\n%s: %s",
-                "Empleado base chofer", obtenerPrimerNombre(),obtenerApellidoPaterno(), 
-                 "NSS",obtenerNumeroSeguroSocial(),
-                 "Sueldo base mas viaticos",( SalarioBase+ViaticosAndCombustible),
-                 "Biaticos",ViaticosAndCombustible,
-                 "Tipo de gasolina",TipoGasolina);
+        return String.format("%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f\n%s: %s\n%s",
+                     "Empleado base chofer", obtenerPrimerNombre(), obtenerApellidoPaterno(), 
+                                                      "NSS", obtenerNumeroSeguroSocial(),
+                "Sueldo base mas viaticos", ( SalarioBase+ViaticosAndCombustible),
+                                               "Biaticos", ViaticosAndCombustible,
+                                "Tipo de gasolina", TipoGasolina,
+                            "------------------------------------------------------------------");
     }
     
    
